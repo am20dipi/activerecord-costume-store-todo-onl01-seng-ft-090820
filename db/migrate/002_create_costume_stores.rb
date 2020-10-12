543e9c610da6564725e67d99fba9e8daf1d23fc0
Adriana DiPietro <am20dipi@alum.siena.edu>
2020-10-12 23:44:25 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -2,5 +2,16 @@
 
 class CreateCostumeStores < ActiveRecord::Base[4.2]
   
+   def change 
+    create_table :costume_store do |t|
+      t.string :name
+      t.string :location
+      t.integer :costume_inventory
+      t.integer :employees
+      t.boolean :business
+      t.timestamp :opening_time
+      t.timestamp :closing_time
+    end
+  end
   
 end
