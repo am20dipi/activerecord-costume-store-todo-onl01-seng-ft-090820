c06f81818a2ed4ed7a7451c8cda05d85a383bb12
Adriana DiPietro <am20dipi@alum.siena.edu>
2020-10-12 23:29:29 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -13,7 +13,8 @@ class CreateCostumes < ActiveRecord::Migration[4.2]
   def change 
     create_table :costumes do |t|
       t.string :name
-      t.integer :price 
+      t.integer :price
+      t.url :image_url
     end
   end
 end 
