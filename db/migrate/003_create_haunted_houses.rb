9dab0a2e73d2c69b89c3741a6c8f1b0117db7b67
Adriana DiPietro <am20dipi@alum.siena.edu>
2020-10-12 23:46:55 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,3 +1,15 @@
 # Create your haunted_houses migration here
 
 class CreateHauntedHouses < ActiveRecord::Base[4.2]
+  
+   def change 
+    create_table :haunted_houses do |t|
+      t.string :name
+      t.string :locatio
+      t.text :image_url
+      t.integer :size 
+      t.timestamp :created_at
+      t.timestamp :updated_at
+    end
+  end
+end
